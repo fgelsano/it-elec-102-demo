8c3064c93b4803eb5816732a68be69b6d60ead1b
Francis Gelsano <2744434+fgelsano@users.noreply.github.com>
2022-03-21 19:44:36 +0800

    Created the view for single blog

--- single.php
+++ single.php
@@ -1,11 +1,30 @@
 <?php
 
-    include 'config/db.php';
+include 'config/db.php';
 
-    $sql = "SELECT * FROM blogs";
-    $result = $conn->query($sql);
+$blog_id = $_GET['id'];
+
+
+$query = mysqli_query($conn, "SELECT * FROM blogs WHERE id=$blog_id") or die(mysqli_error($conn));
+
+
+$row = mysqli_fetch_assoc($query);
+
+$title = $row['title'];
+$description = $row['description'];
+$img = $row['blog_img'];
+
+$formattedDate = date("d M, Y h:i A", strtotime($row['posted_date']));
+
+$user_id = $row['user_id'];
+
+$userQuery = mysqli_query($conn, "SELECT * FROM users WHERE id=$user_id") or die(mysqli_error($conn));
+$userRow = mysqli_fetch_assoc($userQuery);
+
+$author = $userRow['username'];
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +33,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <title>IT Elec 102 Exam Demo</title>
 </head>
 <body>
@@ -23,7 +43,7 @@
             <form class="d-flex">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                 </ul>
                 <button class="btn btn-success text-white" type="submit">Login</button>
@@ -33,29 +53,15 @@
     </nav>
 
     <div id="main-content" class="container mt-4">
-        <div class="row">
-
-        <?php if ($result->num_rows > 0) {  ?>
-                <?php while($row = $result->fetch_assoc()) { ?>
-                    <div class="col-12 col-sm-6 col-md-3 mt-4">
-                        <div class="card">
-                            <img src="assets/uploads/<?php echo $row['blog_img'] ?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $row['title'] ?></h5>
-                                <p class="card-text"><?php echo substr($row['description'], 0, 100) . ' ...' ?></p>
-                                <a href="single.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-        <?php } else { ?>
-            <div class="alert alert-danger" role="alert">
-                Sorry, there are no blogs at the moment!
-            </div>
-        <?php } ?>
-        <?php $conn->close(); ?>
-            
+        <div id="blog-img" style="background-image: url('assets/uploads/<?php echo $img ?>')"></div>
+        <div class="blog-title">
+            <h1 class="mt-3 mb-0"><?php echo $title ?></h1>
+            <small>Posted on: <?php echo $formattedDate ?></small>
+            <p id="author" class="mb-0">Written by: <strong><?php echo $author ?></strong></p>
         </div>
+        <p id="blog-body" class="mt-3">
+            <?php echo $description ?>
+        </p>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
